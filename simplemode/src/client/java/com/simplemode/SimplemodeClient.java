25e32434de709d4191b5c062cd84ac97fbf2dff6
Hippo Maru <hippomaru@ya.ru>
2024-02-15 08:37:01 +0300

    empty mod set up

---
+++ simplemode/src/client/java/com/simplemode/SimplemodeClient.java
@@ -0,0 +1,10 @@
+package com.simplemode;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SimplemodeClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		
+	}
+}
